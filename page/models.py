d3c57a51f3a8b1eb882843b7921e8b46bc773128
BenjaminWuff <b.paredes@duocuc.cl>
2022-06-22 10:31:00 -0400

    navbar + catalogo

--- page/models.py
+++ page/models.py
@@ -4,8 +4,20 @@ from django.db import models
 
 class Usuario(models.Model):
     email = models.EmailField(primary_key=True)
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=40)
     password = models.CharField(max_length=15)
 
     def __str__(self):
         return self.nombre
+
+class Producto(models.Model):
+    id_producto = models.IntegerField(primary_key=True)
+
+    imagen_producto = models.CharField(max_length=150)
+    categoria_producto = models.CharField(max_length=50)
+    nombre_producto = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre_producto
