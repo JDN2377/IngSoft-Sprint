8cb6628759fd94ef3b2223e46fae72830cc05046
BenjaminWuff <b.paredes@duocuc.cl>
2022-07-02 18:42:02 -0400

    Solicitudes

--- page/models.py
+++ page/models.py
@@ -4,8 +4,57 @@ from django.db import models
 
 class Usuario(models.Model):
     email = models.EmailField(primary_key=True)
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=40)
     password = models.CharField(max_length=15)
+
 
     def __str__(self):
         return self.nombre
+
+class Solicitudes(models.model):
+    idsolicitud = models.IntegerField(primary_key=True, verbose_name='Id de solicitud')
+    producto_solicitado = models.CharField(max_length=60, verbose_name='Producto solicitado')
+    cantidad_deseada = models.IntegerField(verbose_name='Cantidad deseada')
+
+    def __str__(self):
+        return self.producto_solicitado
+
+class Audifonos(models.Model):
+    idAudifonos =models.IntegerField(primary_key=True, verbose_name='Id de Audifonos')
+    nombreAudifonos =models.CharField(max_length=50, verbose_name='Nombre de Audifonos')
+    imagenAudifonos =models.CharField(max_length=150, verbose_name='Imagen de Audifonos')
+    precioAudifonos =models.IntegerField(verbose_name='Precio de Audifonos')
+    stockAudifonos =models.IntegerField(verbose_name='Stock de Audifonos')
+
+    def __str__(self):
+        return self.nombreAudifonos
+
+class Pilas(models.Model):
+    idPilas =models.IntegerField(primary_key=True, verbose_name='Id de Pilas')
+    nombrePilas =models.CharField(max_length=50, verbose_name='Nombre de Pilas')
+    imagenPilas =models.CharField(max_length=150, verbose_name='Imagen de Pilas')
+    precioPilas =models.IntegerField(verbose_name='Precio de Pilas')
+    stockPilas =models.IntegerField(verbose_name='Stock de Pilas')
+
+    def __str__(self):
+        return self.nombrePilas
+
+class Microfonos(models.Model):
+    idMicrofonos =models.IntegerField(primary_key=True, verbose_name='Id de Microfonos')
+    nombreMicrofonos =models.CharField(max_length=50, verbose_name='Nombre de Microfonos')
+    imagenMicrofonos =models.CharField(max_length=150, verbose_name='Imagen de Microfonos')
+    precioMicrofonos =models.IntegerField(verbose_name='Precio de Microfonos')
+    stockMicrofonos =models.IntegerField(verbose_name='Stock de Microfonos')
+
+    def __str__(self):
+        return self.nombreMicrofonos
+
+class Reloj(models.Model):
+    idReloj =models.IntegerField(primary_key=True, verbose_name='Id de Reloj')
+    nombreReloj =models.CharField(max_length=50, verbose_name='Nombre del Reloj')
+    imagenReloj =models.CharField(max_length=150, verbose_name='Imagen del Reloj')
+    precioReloj =models.IntegerField(verbose_name='Precio del Reloj')
+    stockReloj =models.IntegerField(verbose_name='Stock del Reloj')
+
+    def __str__(self):
+        return self.nombreReloj
--- page/views.py
+++ page/views.py
@@ -1,8 +1,11 @@
-from .models import Usuario
+from django.urls import reverse_lazy
+from .models import Usuario, Audifonos, Microfonos, Pilas, Reloj
 from http.client import HTTPResponse
 from urllib import response
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -10,29 +13,62 @@ def login(request):
     return render(request, 'login.html')
 
 def catalogo(request):
-    return render(request,'catalogo.html')
+    audifonos = Audifonos.objects.all()
+    microfonos = Microfonos.objects.all()
+    pilas = Pilas.objects.all()
+    reloj = Reloj.objects.all()
+    datos1 = {'audifonos': audifonos, 'microfonos': microfonos, 'pilas': pilas, 'reloj': reloj}
+    return render(request,'catalogo.html', datos1)
+
+def signup(request):
+    return render(request,'signup.html')
+
+def perfil(request):
+    return render(request,'perfil.html')
+
+def solicitudes(request):
+    return render(request,'solicitudes.html')
+
+def register(request):
+    r_correo = request.POST.get('email')
+    r_nombre = request.POST.get('nombre')
+    r_pass = request.POST.get('password')
+    if len(r_correo)>0 and len(r_nombre)>0 and len(r_pass)>0:
+        r_User = Usuario(email=r_correo, nombre=r_nombre, password=r_pass)
+        r_User.save()
+        messages.success(request, 'La cuenta ha sido creada!')
+        return HttpResponseRedirect('login')
+    else:
+        messages.error(request, 'La cuenta ya existe o hubo un error al ingresar los datos.')
+        return HttpResponseRedirect('signup')
+
+def cerrarsesion(request):
+    if 'usuario' in request.session:
+        logout(request)
+        return redirect('/login')
+    else:
+        return redirect('/login')
 
 
-#--Funcion para validar el usuario--
+
+#--Funcion para login del usuario--
 def validarusuario(request):
     # Recibimos los datos del formulario via POST
     v_correo = request.POST.get('email')
     v_pass = request.POST.get('password')
-   
     try:
     #Buscamos el usuario en la base de datos
         usu=Usuario.objects.get(email=v_correo, password=v_pass)
 
-        
         if usu:
             request.session['usuario'] = v_correo
-            return redirect('/catalogo')
-
+            return redirect('/perfil')
     except:
         return redirect('/login')
-#Esta funcion evita entrar a catalogo directo
+
+#Esta funcion evita entrar a perfil directo
 def perfil(request):
     if 'usuario' not in request.session:
         return redirect('/login')
-
-    return render(request, 'catalogo.html')
+    else:
+        return render(request, 'perfil.html')
